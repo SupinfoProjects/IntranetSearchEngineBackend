bbf9b3d4a6ef474769eeffd7e4a5c63140db7682
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-04-10 18:22:28 +0200

    Create UserInfos.h

---
+++ Marine/UserInfos.h
@@ -0,0 +1,42 @@
+#include <string>
+
+static const int authorizedCountriesArrayLen = 1;
+static const int authorizedRegionsArrayLen   = 2;
+
+// Si l'IP de l'utilisateur ne mène pas dans l'un de ces pays; alors rejet de la requête
+static std::string authorizedCountries[] = {
+						std::string("France")
+					};
+
+// Si l'IP de l'utilisateur ne mène pas dans l'une de ces régions; alors rejet de la requête
+static std::string authorizedRegions[] = { 
+						std::string("Rhône-Alpes");
+						std::string("Île-de-France")
+					};
+
+namespace authentification {
+
+void initializeDatabase();
+
+class UserInfos
+{
+public:
+	// Parse un xml pour récupérer infos
+	UserInfos(std::string _xmlFilePath = "");
+	bool parseXmlFile(std::string _xmlFilePath);
+	
+	// Renvoie si l'utilisateur est autorisé ou non (si IP non valide; alors non autorisé)
+	bool isUserAuthorized();
+	
+	// Accesseurs
+	std::string getIpAddress() const;
+	std::string getCountryName() const;
+	std::string getRegionName() const;
+	
+private:
+	std::string ipAddress;
+	std::string countryName;
+	std::string regionName;
+};
+
+}
