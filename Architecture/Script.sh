afb1c5f3b622435a1590abb0c67f69e2ecf4faaf
Dark-pho3nix <charly.faraud@gmail.com>
2014-04-12 18:05:58 +0200

    Création dossier Script Archi
    Les liens pour les scripts et mise en place du script.sh

---
+++ Architecture/Script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Cluster Search Engine Installer
+
+# This script will only work on Debian-based systems. It isn't bulletproof but
+# it will probably work if you simply want to setup a Search Engine Cluster on your Debian/Ubuntu
+# Network. It has been designed to be as unobtrusive and universal as possible.
