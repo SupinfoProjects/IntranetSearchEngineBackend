409cc66432afb3c62068d397db1684ab91878faa
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-05-27 11:26:57 +0200

    Update HtmlParser.h

--- backend/HtmlParser/HtmlParser.h
+++ backend/HtmlParser/HtmlParser.h
@@ -5,17 +5,13 @@
 #include <string>
 #include <vector>
 
-namespace {
-	//std::map<std::string, int> keywords = {};
-}
-
 class HtmlParser
 {
 public:
 	HtmlParser(std::string url = "");
 	int getValueOf(std::string url, std::vector<std::string> keywords);
 private:
-	//std::vector<WebPage> pages;
+
 };
 
 #endif
