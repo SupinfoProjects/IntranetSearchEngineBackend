79d6b8787b58c9bd786640653cb65ca103f10e08
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-06-06 11:09:31 +0200

    Update main.cpp

--- backend/XmlParser/beta/main.cpp
+++ backend/XmlParser/beta/main.cpp
@@ -1,12 +1,24 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include "XmlParserh.h"
 
 int main(int argc, char *argv)
 {
 	XmlParser parser("oc.html");
-	parser.ParseXmlFile();
-	std::cout << parser.mark;
+	//std::cout << parser.mark;
+	for (auto k : parser.get_keywords())
+	{
+		std::cout << k << std::endl;
+	}
+	std::cout << parser.get_mark();
+	/*
+	std::regex r("^(http|https)://[a-z0-9\-\_\\.]*.(com|fr|org|php|htm|html)(/*)[a-z0-9\-\_\\./]*$");
+	std::string s = "https://site-du_zero.com/zboub/aga";
+	if (std::regex_match(s, r))
+	{
+		std::cout << "aga";
+	}*/
 	std::cin.get();
 	return 0;
 }
