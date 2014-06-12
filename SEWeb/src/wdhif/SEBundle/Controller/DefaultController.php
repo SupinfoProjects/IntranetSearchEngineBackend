794217a46fc99cb7e3751b912e8d7179511b5c6a
Wassim Dhif <wassimdhif@gmail.com>
2014-06-12 20:43:45 +0200

    -Paginator finished
    Signed-off-by: Wassim Dhif <wassimdhif@gmail.com>

--- SEWeb/src/wdhif/SEBundle/Controller/DefaultController.php
+++ SEWeb/src/wdhif/SEBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace wdhif\SEBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\False;
+use wdhif\SEBundle\Form\SearchType;
+
 
 class DefaultController extends Controller
 {
@@ -12,9 +16,26 @@ class DefaultController extends Controller
      * @Route("/", name="search")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return array();
+        $error = false;
+        $form = $this->createForm(new SearchType());
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->get('search')->getData();
+            json_encode($data);
+            exec('SEClient', $result, $error);
+            if (isset($result[0])){
+                $result = json_decode($result[0]);
+                var_dump($result);
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'error' => !($error == 0),
+        );
     }
     /**
      * @Route("/result", name="result")
@@ -31,7 +52,7 @@ class DefaultController extends Controller
         $pagination = $paginator->paginate(
             $pages,
             $this->get('request')->query->get('page', 1)/*page number*/,
-            10/*limit per page*/
+            $this->get('request')->query->get('limit', 10)/*limit per page*/
         );
         return array("pagination" => $pagination );
     }
