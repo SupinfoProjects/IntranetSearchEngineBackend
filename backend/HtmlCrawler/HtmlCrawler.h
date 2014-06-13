eb0dcd79557061bd4382813156d55ef649f18f49
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-06-13 12:11:49 +0200

    Update HtmlCrawler.h

--- backend/HtmlCrawler/HtmlCrawler.h
+++ backend/HtmlCrawler/HtmlCrawler.h
@@ -1,10 +1,19 @@
 #ifndef HTML_CRAWLER
 #define HTML_CRAWLER
 
+#include <string>
+#include <vector>
+
 #include "../XmlParser/XmlParser.h"
 
 class HtmlCrawler
 {
+public:
+    HtmlCrawler(std::string url);
+    // ajouter requête bdd
+private:
+    std::vector<XmlParser> parsers;
+    std::vector<std::pair<std::string, bool>> urls; // [url][matched?]
 };
 
 #endif
