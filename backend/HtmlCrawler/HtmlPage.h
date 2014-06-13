f9029fa89068006b9a8e9adb820a3b4063d3ec33
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-06-13 12:38:28 +0200

    Create HtmlPage.h

---
+++ backend/HtmlCrawler/HtmlPage.h
@@ -0,0 +1,14 @@
+#ifndef HTML_PAGE
+#define HTML_PAGE
+
+#include <string>
+#include <vector>
+
+struct HtmlPage
+{
+    std::string url;
+    std::vector<std::string> metaKeywords;
+    int mark;
+};
+
+#endif
