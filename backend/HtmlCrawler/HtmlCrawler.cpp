a21abd95612dcc90422d28ee29c64f14aa0c3e3a
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-06-13 13:48:42 +0200

    Update HtmlCrawler.cpp

--- backend/HtmlCrawler/HtmlCrawler.cpp
+++ backend/HtmlCrawler/HtmlCrawler.cpp
@@ -8,52 +8,27 @@ HtmlCrawler::HtmlCrawler(std::string url)
 
 void HtmlCrawler::crawlWebSite(std::string url)
 {
-    XmlParser parser(url);
-    urls = parser.getUrls();
-    HtmlPage page;
-    page.url = url;
-    page.mark = parser.getMark();
-    page.urls = parser.getUrls();
-    page.metaKeywords = parser.getMetaKeywords();
-    pages.push_back(page);
-    while (true)
+    urls.push_back(url);
+    while (urls.size() > 0)
     {
-        bool areAllUrlsChecked = true;
-        for (auto page : pages)
-        {
-            if (page.mark == UNCHECKED)
-            {
-                areAllUrlsChecked = false;
-            }
-        }
-        if (areAllUrlsChecked)
-        {
-            break;
-        }
         for (auto url : urls)
         {
-            if (url.mark == UNCHECKED)
-            {
-                parser.parseXmlFile(url);
-                HtmlPage page;
-                page.url = url;
-                page.mark = parser.getMark();
-                page.urls = parser.getUrls();
-                page.metaKeywords = parser.getMetaKeywords();
-                pages.push_back(page);
-            }
+            XmlParser parser(url);
+            HtmlPage page;
+            page.url = url;
+            page.mark = parser.getMark();
+            page.urls = parser.getUrls();
+            page.metaKeywords = parser.getMetaKeywords();
+            pages.push_back(page);
         }
-        urls.clear();
+        //urls.clear();
         for (auto page : pages)
         {
-            for (auto url : page.urls)
-            {
-                 if (std::find(urls.begin(), urls.end(), url) != v.end())
-                 {
-                     continue;
-                 }
-                 urls.push_back(url);
-            }
+             if (std::find(urls.begin(), urls.end(), page.url) != v.end())
+             {
+                 continue;
+             }
+             urls.push_back(page.url);
         }
     }
 }
